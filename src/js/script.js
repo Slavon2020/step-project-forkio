82abcf84afc90f41d17f00530f8502e19eb0fec5
KraVit <vitalkram@gmail.com>
2020-12-13 01:36:42 +0200

    hero | adaptive section

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,8 @@
 const menuBurg = document.querySelector('.burger');
+
+const navMobile = document.querySelector('.hero-nav--mobile');
+
 menuBurg.addEventListener('click', function() {
     menuBurg.classList.toggle('burger--opened')
+    navMobile.classList.toggle('hero-nav--mobile-active')
 });
