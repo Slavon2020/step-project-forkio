9a883bc455711a5f7c57d56c3a45caa00d397669
Slavon2020 <vereschaka.slava@gmail.com>
2020-12-15 12:54:13 +0200

    vk-reviews | nav mobile closing fixed

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,18 @@
 const menuBurg = document.querySelector('.burger');
 const navMobile = document.querySelector('.hero-nav--mobile');
 
+function navMobileToggle () {
+    menuBurg.classList.toggle('burger--opened');
+    navMobile.classList.toggle('hero-nav--mobile-active');
+};
+
 menuBurg.addEventListener('click', function() {
-    menuBurg.classList.toggle('burger--opened')
-    navMobile.classList.toggle('hero-nav--mobile-active')
+    navMobileToggle();
 });
 
-//
-// document.body.addEventListener('click', function (e) {
-//     const target = e.target;
-//     if ( navMobile.classList.contains('hero-nav--mobile-active') && target !== menuBurg && target !== navMobile)
-//     // navMobile.classList.remove('hero-nav--mobile-active')
-//     console.log(target);
-//     console.log(menuBurg);
-// });
+document.body.addEventListener('click', function (e) {
+    const target = e.target;
+    if (navMobile.classList.contains('hero-nav--mobile-active') && !navMobile.contains(target) && !menuBurg.contains(target)) {
+        navMobileToggle();
+    }
+});
